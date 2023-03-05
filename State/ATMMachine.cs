3e90be8eeef5f3e96574d6968e58fcb7e37cc2cf
Aniket Kadam <aniketa2k@gmail.com>
2023-03-05 22:43:25 +0530

    Added state design pattern

---
+++ State/ATMMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public class ATMMachine : ATMState
+    {
+        public ATMState atmMachineState { get; set; }
+        public ATMMachine()
+        {
+            atmMachineState = new DebitCardNotInsertedState();
+        }
+        public void InsertDebitCard()
+        {
+            atmMachineState.InsertDebitCard();
+            
+            if (atmMachineState is DebitCardNotInsertedState)
+            {
+                atmMachineState = new DebitCardInsertedState();
+                
+            }
+        }
+        public void EjectDebitCard()
+        {
+            atmMachineState.EjectDebitCard();
+            
+
+            if (atmMachineState is DebitCardInsertedState)
+            {
+                atmMachineState = new DebitCardNotInsertedState();
+                
+            }
+        }
+        public void EnterPin()
+        {
+            atmMachineState.EnterPin();
+        }
+        public void WithdrawMoney()
+        {
+            atmMachineState.WithdrawMoney();
+        }
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initially ATM Machine in DebitCardNotInsertedState
+            ATMMachine atmMachine = new ATMMachine();
+
+            atmMachine.InsertDebitCard();
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+
+            //Trying to move to another state
+            atmMachine.InsertDebitCard();
+
+            atmMachine.EjectDebitCard();
+           
+            Console.Read();
+        }
+    }
+}
